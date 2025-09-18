6767bc03ee725576b1205827cfe3f9de773ba11c
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-09-18 17:04:41 +0900

    Update landing_form_check.php

--- adm/landing_form_check.php
+++ adm/landing_form_check.php
@@ -1,7 +1,7 @@
 <?php
 include_once('./_common.php');
 
-$sql = "SELECT COUNT(*) cnt FROM {$g5['landing']} WHERE (1) AND ld_page = '{$ld_page}' ";
+$sql = "SELECT COUNT(*) cnt FROM {$g5['landing']} WHERE (1) AND ld_page <> 'basic' AND ld_page = '{$ld_page}' ";
 $row = sql_fetch($sql);
 
 if($row['cnt'] > 0){
@@ -9,5 +9,6 @@ if($row['cnt'] > 0){
 }else{
     $arr_result = array('state' => 'success', 'msg' => '사용 가능한 아이디 입니다.');
 }
+
 
 die(json_encode($arr_result));
