1460c6fdd76d3be93965c6b47f604627a8e54450
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-10-02 01:50:59 +0900

    Update index.php

--- landing/index.php
+++ landing/index.php
@@ -11,7 +11,8 @@ $is_use = $row['ld_use'];
 <div class="landing-wrap">
     <h1>Default Landing Page</h1>
     <?php if ($ld_page == null) :?>
-    <p>기본 래딩페이지 입니다. 랜딩 ID를 지정하지 않으셨습니다.</p>
+    <p>기본 래딩페이지 입니다.</p>
+    <p>랜딩페이지 ID를 지정하지 않으셨습니다.</p>
     <?php elseif ($is_use == 0) :?>
     <p>랜딩페이지가 '사용안함'으로 되어 있습니다.</p>
     <?php endif; ?>
@@ -21,7 +22,8 @@ $is_use = $row['ld_use'];
 <?php ob_start() ;?>
     <div class="landing-wrap">
         <h1>Default Landing Page</h1>
-        <p>이 페이지는 랜딩 페이지 파일이 없을 때 보여지는 기본 랜딩페이지입니다.</p>
+        <p>기본 랜딩페이지입니다.</p>
+        <p>랜딩페이지 파일이 없습니다.</p>    
         <?php if($is_admin === 'super'):?>
             <p>랜딩페이지 스킨을 생성하지 않은 경우, 스킨 생성을 먼저 하십시오.</p>
         <?php endif; ?>
@@ -89,3 +91,4 @@ add_stylesheet('<link rel="stylesheet" href="'.$skin_url.'/style.css">', 0);
 
 // 출력
 include $skin_path.'/landing.skin.php';
+
