f65aeaf782e5e65956204ead03400f9e90beb870
semantic-code <73548323+semantic-code@users.noreply.github.com>
2025-10-02 09:39:36 +0900

    Update landing_form_update.php

--- adm/landing_form_update.php
+++ adm/landing_form_update.php
@@ -63,6 +63,8 @@ if($w === ''){
         // 삭제요청 파일 삭제
         $sql = "SELECT bf_no FROM {$g5['board_file_table']} WHERE bo_table = '{$bo_table}' AND wr_id = '{$ld_id}' ";
         $result = sql_query($sql);
+        $keep_file = isset($keep_file) ? $keep_file : array();
+        
         while($row = sql_fetch_array($result)){
             if (!in_array($row['bf_no'], $keep_file)) {
                 delete_attach_file($bo_table, $ld_id, $row['bf_no']);
@@ -80,5 +82,6 @@ if($w === ''){
         alert('데이터 저장에 실패했습니다.');
     }
 }
+
 
 
